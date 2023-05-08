a3e6684c547426b4bf533388a497e181faf133c0
luciarebolino <luciarebolino@gmail.com>
2023-05-07 22:03:29 -0400

    drawing machine

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ $('document').ready(function(){
 
 	$('.column').mouseenter(function(){
 		$(this).css('width', '50%');
-		$('#info').css('width', '3.57142857%');
+		$('#info').css('width', '4.16666667%');
 	});
 	$('.column').mouseleave(function(){
 		$(this).css('width', '3.57142857%');
-		$('#info').css('width', '50%');
+		$('#info').css('width', '4.16666667%');
 	});
 
 	$('.column').css('height', hoogte);
@@ -48,36 +48,39 @@ $('document').ready(function(){
 			// Cloud Machine
 			var project= ['https://docs.google.com/document/d/e/2PACX-1vRniq7Ab8XNmw3Y9I1-lZUIIbo8tR1kWJOJs0-jACxrbKM2i7HlvbUEaK0l2DtU8UgnsTAf-13cpkhP/pub?embedded=true'];
 		} else if (frameClass === 'col-4') {
+			// Drawing Machine
+			var project= ['https://docs.google.com/document/d/e/2PACX-1vRniq7Ab8XNmw3Y9I1-lZUIIbo8tR1kWJOJs0-jACxrbKM2i7HlvbUEaK0l2DtU8UgnsTAf-13cpkhP/pub?embedded=true'];
+		} else if (frameClass === 'col-5') {
 			// Bordo Clima book
 			var project = ['https://docs.google.com/document/d/e/2PACX-1vTU0-N6QLi1wIryL-QC5wXZzjTsBsl00LgK3HRXvYubYbKUjONcZGLy8W32F40bw04p2HJXOJh7YXiL/pub?embedded=true'];
-		} else if (frameClass === 'col-5') {
+		} else if (frameClass === 'col-6') {
 			// Bordo Clima website
 			var project = ['https://docs.google.com/document/d/e/2PACX-1vQZVjOhMcmHyJiiEn35qnDB6hX0r02Hbhja4W5uB7hfInV1JI7NDKu9LICBgEjE8KgXJ7mtLe5E1iMQ/pub?embedded=true'];
-		} else if (frameClass === 'col-6') {			
+		} else if (frameClass === 'col-7') {			
 			// Risograph
 			var project = ['https://docs.google.com/document/d/e/2PACX-1vQOO7BkLmIx7PeyVS15ItlOd_ry3p1hTiA8vspfzbayA_w6BxWoask1XlTnkshZNz4MjJIfRZJlDayW/pub?embedded=true'];
-		} else if (frameClass === 'col-7') {		
+		} else if (frameClass === 'col-8') {		
 			// Something in the air
 			var project = ['https://docs.google.com/document/d/e/2PACX-1vR7JLDyLylF2V7DtPBF8ZmronOrGpZJf-R2FkBdQwN24BttG3FtbjbVAF1wCl7KsWMBME9ijw5-0Yqd/pub?embedded=true'];
-		} else if (frameClass === 'col-8') {
+		} else if (frameClass === 'col-9') {
 			// Polar Opacity
 			var project = ['https://docs.google.com/document/d/e/2PACX-1vQaDLS_fsaJSlMxfTB-QCjXxB0NqImto8sahd1jzOwd152VKidZOLCPmP1DAHJH55D33Vcg4nrS3imo/pub?embedded=true'];
-		} else if (frameClass === 'col-9') {
+		} else if (frameClass === 'col-10') {
 			// Fluid Geographies
 			var project = ['https://docs.google.com/document/d/e/2PACX-1vT6H7gw025pAe7OcDJ_OjK9xnRfpO2Tu4RezxYksEgPJNb0JDzZvTs1R4LPP2LQQUCf3Zkcv2NubWSf/pub?embedded=true'];
-		} else if (frameClass === 'col-10') {
+		} else if (frameClass === 'col-11') {
 			// Performative Calligraphies
 			var project = ['https://docs.google.com/document/d/e/2PACX-1vSmicw7nNSKdZKv-BeuD1II5QvdwlVM3OH7teFD-L0mek3B3nzIq3W1fL9CPkpod9tTo_oYKTaHjSBF/pub?embedded=true'];
-		} else if (frameClass === 'col-11') {
+		} else if (frameClass === 'col-12') {
 			// Scrolling Atmosphere
 			var project = ['https://docs.google.com/document/d/e/2PACX-1vS3GEtp8Lb0pe8r6PAO8YQlo2yJs5ot51M3ev8gHNeuVJLytNblkAL8bBs8gsHthrqG7zR59VcYHblb/pub?embedded=true'];
-		} else if (frameClass === 'col-12') {
+		} else if (frameClass === 'col-13') {
 			// Bluedot
 			var project= ['https://docs.google.com/document/d/e/2PACX-1vSmaaLXQya8VKyv3C1JdW6rkqNWh1FCsJNAihrRo5VkHqzdekK_k2BtB0ffMxq45Oz70kNaZkZS9dF0/pub?embedded=true'];
-		} else if (frameClass === 'col-13') {
+		} else if (frameClass === 'col-14') {
 			// Unseen
 			var project = ['https://docs.google.com/document/d/e/2PACX-1vSmseaDZuC828geyXnES3hddqZgiTUDj8PLN5rhFoSJG2PIV962_oOPTSwtR4gOt8dUmqHzyuBJh2YW/pub?embedded=true'];
-		} else if (frameClass === 'col-14') {
+		} else if (frameClass === 'col-15') {
 			// Eye
 			var project = ['https://docs.google.com/document/d/e/2PACX-1vTAEFDLHfzJdquIhCTbl7IYxMgdfeuLF9_Z62w_0npVHF_EXOVRqGsMApb_YKGnFW17BO-jxy7G0OEG/pub?embedded=true'];
 		} 
